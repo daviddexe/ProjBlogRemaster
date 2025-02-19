9083dfaa8bd937079039323e23fc82bd7f92ba02
daviddexe <gitdavidhub@gmail.com>
2025-02-18 21:28:10 -0300

    finalizado estrutura ( mobile ) - resta card apresentacao

---
+++ main.js
@@ -0,0 +1,22 @@
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+if (hamburger && navLinks) {
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+}
+
+
+const myOserver = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const elements = document.querySelectorAll('.hidden')
+elements.forEach((element) => myOserver.observe(element))
