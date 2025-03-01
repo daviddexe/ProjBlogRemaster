1949dabdb5d6ccbbb2eaa6d36b892eeca47f3da2
daviddexe <gitdavidhub@gmail.com>
2025-03-01 00:11:13 -0300

    configuracao carrossel

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ if (hamburger && navLinks) {
 }
 
 
-const myOserver = new IntersectionObserver ((entries) => {
-    entries.forEach((entry) =>{
-        if(entry.isIntersecting){
+const myOserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add('show')
-        }else{
+        } else {
             entry.target.classList.remove('show')
         }
     })
@@ -26,9 +26,9 @@ elements.forEach((element) => myOserver.observe(element))
 
 
 function sendMail(event) {
-    event.preventDefault();  
+    event.preventDefault();
 
-    
+
     let parms = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
@@ -36,9 +36,9 @@ function sendMail(event) {
         message: document.getElementById("message").value,
     };
 
-  
+
     emailjs.send("service_zvkfgaa", "template_y42kkaz", parms).then(
-        function(response) {
+        function (response) {
             alert("E-mail Enviado com Sucesso!"); // Sucesso
         },
     );
@@ -47,5 +47,27 @@ function sendMail(event) {
 
 document.getElementById("contactForm").addEventListener("submit", sendMail);
 
+const carouselContainer = document.querySelector('.carousel-container');
+const carouselItems = document.querySelectorAll('.carousel-item');
+const prevButton = document.querySelector('.controls button:first-child');
+const nextButton = document.querySelector('.controls button:last-child');
+let currentIndex = 0;
 
+function updateCarousel() {
+    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    carouselItems.forEach((item, index) => {
+        item.classList.toggle('active', index === currentIndex);
+    });
+}
 
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex > 0) ? currentIndex - 1 : carouselItems.length - 1;
+    updateCarousel();
+});
+
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex < carouselItems.length - 1) ? currentIndex + 1 : 0;
+    updateCarousel();
+});
+
+updateCarousel();
